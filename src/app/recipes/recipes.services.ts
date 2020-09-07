c2c3f4533aaca893c95476c0f57ced7b575fc840
AlexandruMTN94 <70522408+AlexandruMTN94@users.noreply.github.com>
2020-09-07 18:55:25 +0300

    Fix to img pozition container, add from recipe

--- src/app/recipes/recipes.services.ts
+++ src/app/recipes/recipes.services.ts
@@ -1,18 +1,37 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
-
+@Injectable()
 export class RecipeService {
+    constructor(private slService: ShoppingListService) {}
     recipeSelected = new EventEmitter<Recipe>();
-    
+
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-        new Recipe('Another Test Recipe', 'This is a simple test',
-            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+        new Recipe('Tasty Schnitzel',
+            'A super-tasty Scnitzel - just awesome!',
+            // tslint:disable-next-line: max-line-length
+            'https://v1.nitrocdn.com/eSLhakvQipAhEWtksvxrnpAZbKWwysTe/assets/static/optimized/rev-744f298/wp-content/uploads/2019/10/pork-schnitzel-3-1170x617.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe('Big Fat Burger',
+            'What else you need to say?',
+            'https://miro.medium.com/max/700/1*Cp88RkZX8hBSZQ3ITeH_Uw.jpeg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
     ];
 
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
