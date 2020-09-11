927b6a18fab564f5b39a4d8ce3624af54b003ee5
AlexandruMTN94 <70522408+AlexandruMTN94@users.noreply.github.com>
2020-09-11 11:24:21 +0300

    Add form reset, add, update and clear functional

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -22,7 +22,11 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
         }
       );
   }
- 
+
+  onEditItem(index: number) {
+    this.slService.startedEditing.next(index);
+  }
+
   ngOnDestroy() {
     this.igChangeSub.unsubscribe();
   }
